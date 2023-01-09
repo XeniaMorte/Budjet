fb70d7b8c0e21443e9909ad3aac94e26158975ca
xenia <ksen.novikova@mail.ru>
2023-01-09 19:43:18 +0300

    Initial commit

---
+++ src/main/java/com/example/budjet/Bud.java
@@ -0,0 +1,2 @@
+package com.example.budjet;public class Bud {
+}
---
+++ src/main/java/com/example/budjet/BudjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.budjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudjetApplication.class, args);
+	}
+
+}
